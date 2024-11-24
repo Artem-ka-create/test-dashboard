11fcc0acb9cd7ceb12c394f47ad6c369a33890a4
Artem Petrenko <artem.petrenko@telekom.com>
2024-11-24 06:16:27 +0100

    feat: main page

--- app/components/Feed.tsx
+++ app/components/Feed.tsx
@@ -1,116 +1,32 @@
-"use client";
-
-import React, { useEffect, useState } from 'react'
-import PromptCard from './PromptCard';
-
-
-interface Post {
-  tag: string;
-  prompt: string;
-  [key: string]: any; // Optional, if there are other unknown properties
-}
-interface PromptCardListProps {
-  data: Post[]; // Expecting an array of posts
-  handleTagClick: (tag: string) => void; // Function to handle tag clicks
-}
-
-const PromptCardList: React.FC<PromptCardListProps> = ({ data, handleTagClick }) => {
-  return (
-    <div className='mt-16 prompt_layout'>
-      {data.map((post: Post) => (
-        <div key={post._id}>
-          {/* Replace with actual content */}
-          <PromptCard post={post} handleTagClick={handleTagClick} />
-        </div>
-      ))}
-    </div>
-  );
-};
-
+import Image from 'next/image'
+import React from 'react'
 
 const Feed = () => {
-
-
-
-  // Search states
-  const [searchText, setSearchText] = useState("");
-  const [searchTimeout, setSearchTimeout] = useState<NodeJS.Timeout | null>(null);
-
-  const [allPosts, setAllPosts] = useState<Post[]>([]);
-  const [searchedResults, setSearchedResults] = useState<Post[]>([]);
-
-
-  const fetchPosts = async () => {
-    
-    const response = await fetch("/api/prompt");
-    console.log(response);
-    
-    const data = await response.json();
-
-    setAllPosts(data);
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
-  // const filterPrompts = (searchtext: string) => {
-  //   const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
-  //   return allPosts.filter(
-  //     (item) =>
-  //       regex.test(item?.tag) ||
-  //       regex.test(item?.prompt)
-  //   );
-  // };
-
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-    
-    if (searchTimeout) {
-      clearTimeout(searchTimeout);
-    }
-
-    setSearchText(e.target.value);
-
-    setSearchTimeout(
-      setTimeout(() => {
-        const searchResult = filterPrompts(e.target.value);
-        setSearchedResults(searchResult);
-      }, 500)
-    );
-  };
-
-  const handleTagClick = (tagName: string) => {
-    setSearchText(tagName);
-
-    const searchResult = filterPrompts(tagName);
-    setSearchedResults(searchResult);
-  };
-
-
   return (
-    <section className='feed'>
-      <form className='relative w-full flex-center'>
-        <input
-          type='text'
-          placeholder='Search for a tag or a username'
-          value={searchText}
-          onChange={handleSearchChange}
-          required
-          className='search_input peer'
-        />
-      </form>
+    <div className="relative">
+      <Image
+        className="relative"
+        src={"wave.svg"}
+        alt="wave"
+        height={1000}
+        width={1000}
+      />
 
-      {/* All Prompts */}
-      {searchText ? (
-        <PromptCardList
-          data={searchedResults}
-          handleTagClick={handleTagClick}
-        />
-      ) : (
-        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
-      )}
-    </section>
+      {/* Nested image */}
+      <Image
+        className="absolute top-20 left-1/2 transform -translate-x-1/2" // Adjust these values for positioning
+        src={"telekom.svg"}
+        alt="Tlogo"
+        height={140}
+        width={140}
+      />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 text-center">
+        <h1 style={{
+          fontSize: "70px !important",}} className="text-8xl font-bold bg-gradient-to-r from-[#e20074] to-[#ff5e99] text-transparent bg-clip-text">
+          LLM Penetration Testing Tool
+        </h1>
+      </div>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,7 @@ import Feed from './components/Feed'
 const Home = () => {
   return (
     <section className="w-full flex justify-center items-center flex-col">
-
-      <h1 className='text-center'>
-        Discover and Share
-        <br className='max-md:hidden' />
-        <span >AI prompts</span>
-      </h1>
-      <p className='desc'> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Optio iste possimus, quas sit molestias incidunt eaque repellendus culpa natus
-      </p>
-
-      <image src='wave.svg' />
+      
       <Feed />
     </section>
   )
